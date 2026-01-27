1193ee31956ba218f180a7b162e6137c55d90991
theHumanBorch <ncborch@gmail.com>
2026-01-27 15:13:16 -0600

    relevel the baslisk environ

--- R/basilisk.R
+++ R/basilisk.R
@@ -14,24 +14,25 @@ immLynxEnv <- basilisk::BasiliskEnvironment(
     pkgname = "immLynx",
     packages = c(
         "python=3.9",
-        "pandas>=2.2.1",
-        "tensorflow",
-        "matplotlib",
-        "svalkiers::clustcr",
-        "pytorch::faiss-cpu",
-        "conda-forge::markov_clustering",
-        "conda-forge::scikit-learn",
-        "conda-forge::numpy>=1.26.4",
-        "conda-forge::scipy>=1.12.0",
-        "conda-forge::statsmodels>=0.14.1",
-        "conda-forge::seaborn"
+        "pandas=2.2.1",
+        "matplotlib=3.8.0",
+        "bioconda::markov_clustering=0.0.6",
+        "svalkiers::clustcr=1.0.3",
+        "pytorch::faiss-cpu=1.7.4",
+        "conda-forge::scikit-learn=1.4.0",
+        "conda-forge::numpy=1.26.4",
+        "conda-forge::scipy=1.12.0",
+        "conda-forge::statsmodels=0.14.1",
+        "conda-forge::seaborn=0.13.2"
     ),
+    channels = c("conda-forge", "bioconda", "svalkiers", "pytorch"),
     pip = c(
-        "tcrdist3",
-        "DeepTCR",
-        "olga",
-        "sonnia",
-        "metaclonotypist",
-        "pyrepseq>=1.5.1"
+        "tcrdist3==0.2.2",
+        "DeepTCR==2.1.1",
+        "olga==1.2.4",
+        "sonnia==0.1.0",
+        "metaclonotypist==0.2.0",
+        "pyrepseq==1.5.1",
+        "tensorflow==2.15.0"
     )
 )
